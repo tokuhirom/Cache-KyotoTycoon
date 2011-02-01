f28a83bd6075a457270b8bac2a3171303cfc02c8
tokuhirom <tokuhirom@gmail.com>
2011-02-01 17:56:54 +0900

    Checking in changes prior to tagging of version 0.12.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index da2d392..17ed359 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension Cache::KyotoTycoon
    
    +0.12
    +
    +    - $cursor->get() returns $xt.(tokuhirom)
    +    - doc enhancements(tokuhirom)
    +
     0.11
    
         - display better error message.

--- lib/Cache/KyotoTycoon.pm
+++ lib/Cache/KyotoTycoon.pm
@@ -2,7 +2,7 @@ package Cache::KyotoTycoon;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 use Cache::KyotoTycoon::Cursor;
 use TSVRPC::Client;
 use Carp ();
